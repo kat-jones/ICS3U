1b89c9d5a8c6bd10350c72d019ae975749e08610
kat12_000 <kat12_000@10.17.194.36>
2017-02-21 11:20:58 -0500

    initial commit

---
+++ myFirstProject/src/jones/HelloWorld.java
@@ -0,0 +1,18 @@
+package jones;
+
+/**
+ * HelloWorld.java
+ * This program prints Hello, World! to the console.
+ * February 21, 2017
+ * @author Kat Jones
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World!");
+
+	}
+
+}
